9397bc2a6d970e3efb897a7af6e55b896f52f4c7
mehedi hasan <126792834+mehexi@users.noreply.github.com>
2023-11-11 21:55:09 +0600

    done

--- src/componants/blog/blog.js
+++ src/componants/blog/blog.js
@@ -1,25 +1,24 @@
 import React from "react";
 import "./blog.css";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookmark } from "@fortawesome/free-regular-svg-icons";
 
 const Blog = (props) => {
-//   console.log(props.blog);
+  //   console.log(props.blog);
   const {
-    id,
     cover,
     title,
     author_img,
     author,
     posted_date,
     hashtags,
-    posted_data,
+
     reading_time,
-    } = props.blog;
-    
-    const markAsRead = props.markAsRead
-    const addToBookmark = props.addToBookmark
+  } = props.blog;
+
+  const markAsRead = props.markAsRead;
+  const addToBookmark = props.addToBookmark;
 
   return (
     <div className="blog">
@@ -34,17 +33,20 @@ const Blog = (props) => {
         </div>
         <div className="reading-details">
           <p>{reading_time} min read</p>
-          <span onClick={()=> addToBookmark(title)}>
+          <span onClick={() => addToBookmark(title)}>
             <FontAwesomeIcon icon={faBookmark}></FontAwesomeIcon>
           </span>
         </div>
-          </div>
-          <h1>{title}</h1>
-          <div className="reading-details">
-              <p>#{hashtags[0]}</p>
-              <p>#{hashtags[1]}</p>
-          </div>
-          <p onClick={()=>markAsRead(reading_time)} className="mark"> <b>Mark as Read</b></p>
+      </div>
+      <h1>{title}</h1>
+      <div className="reading-details">
+        <p>#{hashtags[0]}</p>
+        <p>#{hashtags[1]}</p>
+      </div>
+      <p onClick={() => markAsRead(reading_time)} className="mark">
+        {" "}
+        <b>Mark as Read</b>
+      </p>
     </div>
   );
 };
